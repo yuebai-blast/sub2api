6192a6ae65ebc3d8183613fbe9af9feb066d408a
月白 <yuebai_blast@qq.com>
2026-06-04 00:35:08 +0800

    chore(update-service): 修改自动更新检查的GitHub仓库地址

--- backend/internal/service/update_service.go
+++ backend/internal/service/update_service.go
@@ -28,7 +28,7 @@ var (
 const (
 	updateCacheKey = "update_check_cache"
 	updateCacheTTL = 1200 // 20 minutes
-	githubRepo     = "Wei-Shaw/sub2api"
+	githubRepo     = "yuebai-blast/sub2api"
 
 	// Security: allowed download domains for updates
 	allowedDownloadHost = "github.com"
